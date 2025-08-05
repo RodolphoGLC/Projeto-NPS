b50dcdc9d507e118904d2c37bed443d7c0fea860
RodolphoGLC <rodolpho.geouerj20@gmail.com>
2025-08-05 09:16:59 -0300

    Conexão com o banco do postgres

--- NpsApi/Program.cs
+++ NpsApi/Program.cs
@@ -1,25 +1,20 @@
+using Microsoft.EntityFrameworkCore;
+using NpsApi.Data;
+using System;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Conexão com PostgreSQL
+builder.Services.AddDbContext<NpsDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("PostgresConnection")));
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
+app.UseSwagger();
+app.UseSwaggerUI();
 app.MapControllers();
-
 app.Run();
